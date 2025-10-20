f2ed5e5c4c17c3e5c8f536fbac4a54798bb0bdb7
Quinn Donnelly <quinndonnelly22@gmail.com>
2025-10-20 07:24:44 -0400

    feat: game camera and simple level

---
+++ scene/player/player.gd
@@ -0,0 +1,11 @@
+class_name Player
+extends CharacterBody2D
+
+@export var speed: float = 350
+
+func _physics_process(_delta: float) -> void:
+	var direction: Vector2 = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	velocity = direction * speed
+	move_and_slide()
+
+
