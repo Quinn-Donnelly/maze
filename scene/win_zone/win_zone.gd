a01866d1320a06b6e62672a7679336c4e30ec9ff
Quinn Donnelly <quinndonnelly22@gmail.com>
2025-10-22 06:36:17 -0400

    feat: win zone that will mark where you win the maze

---
+++ scene/win_zone/win_zone.gd
@@ -0,0 +1,10 @@
+class_name WinZone
+extends Area2D
+
+func _ready() -> void:
+	body_entered.connect(self._on_body_entered)
+
+func _on_body_entered(body: Node2D) -> void:
+	if body is Player:
+		print("Gratz")
+	
