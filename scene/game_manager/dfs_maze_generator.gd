607e7c75401f5888ed49a7f44b19bbcbbae11c39
Quinn Donnelly <quinndonnelly22@gmail.com>
2025-10-25 17:23:00 -0400

    feat: created a dfs based maze generator

---
+++ scene/game_manager/dfs_maze_generator.gd
@@ -0,0 +1,43 @@
+class_name DFSMazeGenerator
+extends Node
+
+var directions: Array[Vector2] = [Vector2(0,-2), Vector2(2,0), Vector2(0,2), Vector2(-2,0)]
+
+func generate_dfs_maze(width: int, height: int) -> Array:
+	var grid: Array[Array] = []
+	for row in range(height + 1):
+		var currentRow: Array[int] = []
+		currentRow.resize(width + 1)
+		currentRow.fill(1)
+		grid.append(currentRow)
+
+	var stack = []
+	var row = 1
+	var col = 1
+	grid[row][col] = 0
+	stack.push_front(Vector2(row, col))
+	
+	var current
+	while stack.size() > 0:
+		current = stack.pop_front()
+		directions.shuffle()
+		for dir in directions:
+			if current.x + dir.x >= height || current.x + dir.x <= 0 || current.y + dir.y <= 0 || current.y + dir.y >= width || grid[current.x + dir.x][current.y + dir.y] == 0:
+				continue
+			stack.push_front(current)
+			grid[current.x + dir.x / 2][current.y + dir.y / 2] = 0
+			grid[current.x + dir.x][current.y + dir.y] = 0
+			current.x += dir.x
+			current.y += dir.y
+			stack.push_front(current)
+			
+	pretty_print_2d_array(grid)
+	return grid
+
+func pretty_print_2d_array(array: Array[Array]) -> void:
+	for row in array:
+		var row_string = ""
+		for n in row:
+			row_string += "%d," % n
+		row_string = row_string.erase(row_string.length()-1)u
+		print(row_string)
--- scene/game_manager/game_manager.gd
+++ scene/game_manager/game_manager.gd
@@ -1,4 +1,9 @@
 extends Node
 
+@onready var dfs: DFSMazeGenerator = $DFS
+
+func _ready() -> void:
+	print(dfs.generate_dfs_maze(20, 20))
+
 func get_current_level() -> Level:
 	return get_tree().current_scene as Level
