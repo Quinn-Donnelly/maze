37bd3e2b6dd8b9ba360c68eb06fcf8112eac5791
Quinn Donnelly <quinndonnelly22@gmail.com>
2025-10-26 08:25:02 -0400

    refactor: moved to sprite over map

--- scene/player/player.gd
+++ scene/player/player.gd
@@ -7,4 +7,3 @@ func _physics_process(_delta: float) -> void:
 	var direction: Vector2 = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = direction * speed
 	move_and_slide()
-
