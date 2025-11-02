76de2787ff254fce27060629fc5bc784fde8e409
Quinn Donnelly <quinndonnelly22@gmail.com>
2025-11-02 11:32:16 -0500

    WIP: projectile enemy shooting
    - Still need to put it on a cooldown
    - Need to track entities that enter and exit then on timer cooldown I'll
      shoot at one of the entities being tracked

---
+++ components/projectile.gd
@@ -0,0 +1,18 @@
+class_name Projectile
+extends Area2D
+
+var velocity: Vector2 = Vector2(0,0)
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	area_entered.connect(self._on_area_entered)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	position += velocity * delta
+
+func _on_area_entered(area: Area2D) -> void:
+	if area is not HurtBox:
+		return
+	
+	print("get slapped loser")
---
+++ components/range_attack.gd
@@ -0,0 +1,27 @@
+class_name RangeAttack
+extends Node2D
+
+@export var vision: Area2D
+@export var max_range: float = 200
+@export var min_range: float = 10
+@export var projectile_speed: float = 200
+@export var projectile: PackedScene
+@export var rangeSpawnPoint: Marker2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	vision.area_entered.connect(self._on_vision_area_entered)
+
+func _on_vision_area_entered(area: Area2D) -> void:
+	var distance: float = (area.global_position - self.global_position).length()
+	if distance > max_range or distance < min_range:
+		return
+	
+	print("Hit em where it hurts")
+	_fire(area.global_position)
+
+func _fire(aimAt: Vector2) -> void:
+	var shot: Projectile = projectile.instantiate()
+	shot.global_position = rangeSpawnPoint.global_position
+	shot.velocity = (aimAt - global_position).normalized() * projectile_speed
+	get_tree().current_scene.call_deferred("add_child", shot)
