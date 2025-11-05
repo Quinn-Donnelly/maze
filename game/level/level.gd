f7dec18a89a473e56a2f9fb8fd7e9bfffafdc59d
Quinn Donnelly <quinndonnelly22@gmail.com>
2025-11-05 11:34:48 -0500

    feat: using groups to clean up maze instances
    - opens the way for spawing in enemies and other maze objects

--- game/level/level.gd
+++ game/level/level.gd
@@ -4,12 +4,14 @@ extends Node
 ## Camera used to show player when level power used
 @export var levelCamera: Camera2D
 @export var winningZone: PackedScene
+## For locations which are valid ends but not chosen I'll spawn in randomly from here
+@export var supriseScenes: Array[PackedScene]
 @onready var mazeWalls: MazeWalls = $MazeWalls
 @onready var fog: Fog = $FogOfWar
 @onready var player: Player = $Player
 @onready var spawnLocation: Marker2D = $PlayerSpawnLocation
 
-var currentWinZone: WinZone
+const maze_group = "maze_instances"
 
 func _ready() -> void:
 	start_level()
@@ -18,21 +20,23 @@ func _ready() -> void:
 func get_level_camera() -> Camera2D:
 	return levelCamera
 
-func start_level() -> void:
-	if currentWinZone != null:
-		currentWinZone.queue_free()
-	
+func start_level() -> void:	
 	player.global_position = spawnLocation.global_position
 	if mazeWalls:
 		mazeWalls.generate_maze()
-		_create_winning_location(mazeWalls.get_winning_location())
+		_create_scene_at_location(mazeWalls.get_winning_location(), winningZone)
 	if fog:
 		fog.generate_fog()
 
-func _create_winning_location(globalLocation: Vector2) -> void:
-	currentWinZone = winningZone.instantiate()
-	currentWinZone.global_position = globalLocation
-	add_child(currentWinZone)
+func _create_scene_at_location(globalLocation: Vector2, scene: PackedScene) -> void:
+	var instance = scene.instantiate()
+	instance.global_position = globalLocation
+	instance.add_to_group(maze_group)
+	add_child(instance)
+
+func _free_maze_group() -> void:
+	get_tree().call_group(maze_group, "queue_free")
 
 func _on_play_again() -> void:
+	_free_maze_group()
 	start_level()
